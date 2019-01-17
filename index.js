07a20fe7d2ca3eb0bc55d081e2de4c3d545467b5
austinloveless <austinloveless5171@gmail.com>
2019-01-17 13:02:39 -0700

    full email template functionality

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 4000;
 const nodemailer = require("./lib/nodemailer");
+const methodOverride = require("method-override");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+app.get("/travel", (req, res) => {
+  res.render("travel");
+});
+
+app.get("/email", (req, res) => {
+  res.render("emailForm");
+});
 
 app.post("/email", (req, res, next) => {
+  console.log("body", req.body);
+
   nodemailer.subscribe(req.body).then(response => {
-    res.send("Success");
+    res.send("success");
   });
+  res.render("success");
+});
+
+app.get("/success", (req, res) => {
+  res.render("success");
 });
 
 app.listen(port, () => {
